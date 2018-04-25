0be1c5a324d5ab16fbcebc154bb024c313c034f0
Cody Sherrod <w.cody.sherrod@gmail.com>
2018-04-24 21:48:45 -0500

    initial commit with some files

---
+++ app/friends.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Friends (DATA)
+//==============================================================
+var friends = [
+    {
+        name: "",
+        photo: "???",
+        scores: []
+    }
+]
+
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
